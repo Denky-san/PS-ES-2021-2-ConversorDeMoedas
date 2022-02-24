d40f7385f8530862414eaf781453acff7047bf1a
Matheus <matedits03216@gmail.com>
2022-02-24 06:10:49 -0300

    Ambiente MVC Configurado

---
+++ Todolist/Business/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+
+        public List<Usuario> Usuarios(string nome)
+        {
+            var usuarios = new List<Usuario>();
+            usuarios.Add(new Usuario() { Nome = "Pietro" });
+            return usuarios;
+        }
+    }
+}
---
+++ Todolist/MVC_GerenciadorDeConteudo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_GerenciadorDeConteudo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
