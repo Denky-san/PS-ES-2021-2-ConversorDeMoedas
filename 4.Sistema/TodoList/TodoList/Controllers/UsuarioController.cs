93c816ee1e35f89f11fa36bf95a8f16a4e858190
Matheus <matedits03216@gmail.com>
2022-03-09 23:45:49 -0300

    Configurando SQL

--- 4.Sistema/TodoList/TodoList/Controllers/UsuarioController.cs
+++ 4.Sistema/TodoList/TodoList/Controllers/UsuarioController.cs
@@ -3,14 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TodoList.Models;
+using TodoList.Repositorio;
 
 namespace TodoList.Controllers
 {
     public class UsuarioController : Controller
     {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<UsuarioModel> contatos =_usuarioRepositorio.BuscarTodos();
+
+            return View(contatos);
         }
 
         public IActionResult Criar()
@@ -26,6 +37,14 @@ namespace TodoList.Controllers
         public IActionResult ApagarConfirmacao()
         {
             return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Criar(UsuarioModel usuario)
+        {
+            _usuarioRepositorio.Adicionar(usuario);
+            return RedirectToAction("Index");
         }
     }
 }
