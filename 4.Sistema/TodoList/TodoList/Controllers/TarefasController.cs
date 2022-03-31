7eee3108f5ddd37f4235ef32ca89e36d9ccaea12
Matheus <matedits03216@gmail.com>
2022-03-31 03:40:43 -0300

    Atualizações de Front-End

---
+++ 4.Sistema/TodoList/TodoList/Controllers/TarefasController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoList.Models;
+
+namespace TodoList.Controllers
+{
+    public class TarefasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(TarefasModel tarefa)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _usuarioRepositorio.Adicionar(tarefa);
+                    TempData["MensagemSucesso"] = "Contato cadastrado com sucesso.";
+                    return RedirectToAction("Index");
+                }
+
+                return View(usuario);
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos cadastrar seu usuário. Detalhes do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ 4.Sistema/TodoList/TodoList/Models/TarefasModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Models
+{
+    public class TarefasModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Status { get; set; }
+    }
+}
